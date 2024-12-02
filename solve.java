03c6d1c8e6f745b2ee08739e2749f09dc1329d3b
Dashiel Lareau <133545504+PigochiBC@users.noreply.github.com>
2024-12-02 16:30:18 -0500

    Add files via upload

--- solve.java
+++ solve.java
@@ -10,9 +10,7 @@ public class solve {
     static boolean canRemoveBadCase = true;
     public static void main(String[] args) {
 
-        ArrayList<String> fileData = getFileData("src/input.txt");
-
-
+        ArrayList<String> fileData = getFileData("input.txt");
         for (int i = 0; i < fileData.size(); i++) {
             String[] noSpaces = fileData.get(i).split(" ");
             if (Integer.parseInt(noSpaces[0]) > Integer.parseInt(noSpaces[1])) {
@@ -20,46 +18,9 @@ public class solve {
             } else {
                 decreasing = false;
             }
-            for (int j = 0; j < noSpaces.length - 1; j++) {
-                if ((decreasing && Integer.parseInt(noSpaces[j]) < Integer.parseInt(noSpaces[j + 1]))) {
-                    if(canRemoveBadCase){
-                        noSpaces = handleWhichBadCase(noSpaces,j);
-                        canRemoveBadCase = false;
-                        j=-1;
-                        continue;
-                    }else{
-                        System.out.println("CRAZY SHIT HAPPENED: " + Arrays.stream(noSpaces).toList());
-                        break;
-                    }
-                } else if ((!decreasing && Integer.parseInt(noSpaces[j]) > Integer.parseInt(noSpaces[j + 1]))) {
-                    if(canRemoveBadCase){
-                        noSpaces = handleWhichBadCase(noSpaces,j);
-                        canRemoveBadCase = false;
-                        j=-1;
-                        continue;
-                    }else{
-                        System.out.println("CRAZY SHIT HAPPENED: " + Arrays.stream(noSpaces).toList());
-                        break;
-                    }
-                }
-
-                if (Math.abs(Integer.parseInt(noSpaces[j]) - Integer.parseInt(noSpaces[j + 1])) >= 1 && Math.abs(Integer.parseInt(noSpaces[j]) - Integer.parseInt(noSpaces[j + 1])) <= 3) {
-                    if (j == noSpaces.length - 2) {
-                        safeCases++;
-                        System.out.println("SAFE: " + Arrays.stream(noSpaces).toList());
-                    }
-                } else {
-                    if(canRemoveBadCase){
-                        noSpaces = handleWhichBadCase(noSpaces,j);
-                        canRemoveBadCase = false;
-                        j=-1;
-                    }else{
-                        System.out.println("CRAZY SHIT HAPPENED: " + Arrays.stream(noSpaces).toList());
-                        break;
-                    }
-                }
+            if(handleWhichBadCase(noSpaces)){
+                safeCases++;
             }
-            canRemoveBadCase = true;
         }
         System.out.println(safeCases);
 
@@ -99,48 +60,33 @@ public class solve {
         return arr2;
     }
 
-    public static String[] handleWhichBadCase(String[] input, int problemIndex){
-        String[] returnMe = new String[0];
+    public static boolean handleWhichBadCase(String[] input){
 
-        //test if decreasing is correct
-        int dec = 0;
-        int inc = 0;
-        for (int i = 0; i < input.length - 1; i++) {
-            if (Integer.parseInt(input[i]) > Integer.parseInt(input[i + 1])) {
-                dec++;
-            } else {
-                inc++;
-            }
-        }
-        if (inc > dec) {
-            decreasing = false;
-        } else {
-            decreasing = true;
-        }
-
-        if(decreasing && Integer.parseInt(input[problemIndex]) <= Integer.parseInt(input[problemIndex+1])){
-            returnMe = remove(input, problemIndex);
-        }else if(!decreasing && Integer.parseInt(input[problemIndex]) > Integer.parseInt(input[problemIndex+1])){
-            returnMe = remove(input, problemIndex);
-        }
-
-        if(!(Math.abs(Integer.parseInt(input[problemIndex]) - Integer.parseInt(input[problemIndex + 1])) >= 1 && Math.abs(Integer.parseInt(input[problemIndex]) - Integer.parseInt(input[problemIndex + 1])) <= 3)){
-            try{
-                if(Math.abs(Integer.parseInt(input[problemIndex]) - Integer.parseInt(input[problemIndex+2])) >=1 && (Math.abs(Integer.parseInt(input[problemIndex]) - Integer.parseInt(input[problemIndex+2])) <=3)){
-                    returnMe = remove(input,problemIndex+1);
-                }else{
-                    returnMe = remove(input,problemIndex);
+        for(int i = 0; i < input.length; i++){
+            String[] currentIteration = remove(input, Integer.parseInt(input[i]));
+            for(int j = 0; j < currentIteration.length-1; j++){
+                if(Integer.parseInt(currentIteration[j]) > Integer.parseInt(currentIteration[j+1])){
+                    decreasing=true;
                 }
-            }catch(IndexOutOfBoundsException e){
-                if(Integer.parseInt(input[problemIndex]) > (Integer.parseInt(input[problemIndex+1] + 3))){
-                    returnMe = remove(input,problemIndex);
-                }else if (Integer.parseInt(input[problemIndex+1]) > (Integer.parseInt(input[problemIndex]) + 3)){
-                    returnMe = remove(input,problemIndex+1);
-                }else{
-                    returnMe = remove(input,problemIndex);
+                else{
+                    decreasing = false;
+                }
+                if ((decreasing && Integer.parseInt(currentIteration[j]) < Integer.parseInt(currentIteration[j + 1]))) {
+                    break;
+                } else if ((!decreasing && Integer.parseInt(currentIteration[j]) > Integer.parseInt(currentIteration[j + 1]))) {
+                    break;
+                }
+
+                if (Math.abs(Integer.parseInt(currentIteration[j]) - Integer.parseInt(currentIteration[j + 1])) >= 1 && Math.abs(Integer.parseInt(currentIteration[j]) - Integer.parseInt(currentIteration[j + 1])) <= 3) {
+                    if (j == currentIteration.length - 2) {
+                        return true;
+                    }
+                } else {
+                    break;
                 }
             }
         }
-        return returnMe;
+
+        return false;
     }
 }
