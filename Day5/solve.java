3530e395e4495fd5a2dbc3d803fc13782efab0c7
Dashiel Lareau <133545504+PigochiBC@users.noreply.github.com>
2024-12-05 14:11:22 -0500

    Add files via upload

--- Day5/solve.java
+++ Day5/solve.java
@@ -3,6 +3,7 @@ package Day5;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -10,43 +11,75 @@ public class solve {
     static boolean rulesSwitch = false;
     static ArrayList<String> inputStrings = new ArrayList<>();
     public static void main(String[] args){
-        ArrayList<String> fileData = getFileData("Day5/input.txt");
-        ArrayList<Integer> fileDataJustNums = new ArrayList<>();
-        ArrayList<Integer> fileDataJustRules = new ArrayList<>();
+        ArrayList<String> fileData = getFileData("input.txt");
+        ArrayList<Integer> fileDataJustNums;
+        ArrayList<Integer> fileDataJustRules;
         ArrayList<Integer> currentInputIteration = getNewIteration(inputStrings, 0);
-        System.out.println(currentInputIteration);
         /*
          * Algorithm:
-         *   1. Get just nums in array to its own arraylist
+         *   1. Get just nums in array to its own arraylist, and rules into its own list
          *   2. Remove irrelevent info from nums and rules by comparing to current iteration of input
-         *   3. 
+         *   3. Sort rules array L-G to make sure all number rules are done at the same time
+         *   4. Swap nums array spots based on the rules
+         *   5. if given list = sorted list, total+= middle num.
          */
-        
-        for(int i =0; i < fileData.size(); i++){
-            if(currentInputIteration.contains(Integer.parseInt(fileData.get(i).split("\\|")[0])) && currentInputIteration.contains(Integer.parseInt(fileData.get(i).split("\\|")[1]))){
-                fileDataJustRules.add(Integer.parseInt(fileData.get(i).split("\\|")[0] + "" + fileData.get(i).split("\\|")[1]));
-                fileDataJustNums.add(Integer.parseInt(fileData.get(i).split("\\|")[0]));
-            } 
-        }
-        
-        System.out.println(fileDataJustRules);
-        System.out.println(fileDataJustNums);
-    }
-    public static void sortArray(ArrayList<Integer> input, ArrayList<Integer> rules){
-        for(int i =0; i < input.size(); i++){
+        int total = 0;
+        for(int x =0; x < inputStrings.size(); x++){
+            fileDataJustNums = new ArrayList<>();
+            fileDataJustRules = new ArrayList<>();
+            currentInputIteration = getNewIteration(inputStrings, x);
 
-            for(int j = 0; j < input.size(); j++){
-                //if we find the rule number at index j, check if i > j, telling us if the rule is being followed or not.
-                if(rules.get(i) == input.get(j)){
-                    if(i > j){
-                        int temp = input.get(i);
-                        input.set(i, input.get(j));
-                        input.set(j, temp);
-                    }
-                } 
+            for(int i =0; i < fileData.size(); i++){
+                if(currentInputIteration.contains(Integer.parseInt(fileData.get(i).split("\\|")[0])) && currentInputIteration.contains(Integer.parseInt(fileData.get(i).split("\\|")[1]))){
+                    fileDataJustRules.add(Integer.parseInt(fileData.get(i).split("\\|")[0] + "" + fileData.get(i).split("\\|")[1]));
+                    if(!fileDataJustNums.contains(Integer.parseInt(fileData.get(i).split("\\|")[0]))) fileDataJustNums.add(Integer.parseInt(fileData.get(i).split("\\|")[0]));
+                }
+            }
+            Collections.sort(fileDataJustRules);
+
+            for(int i = 0; i < currentInputIteration.size(); i++){
+                if(!fileDataJustNums.contains(currentInputIteration.get(i))){
+                    fileDataJustNums.add(currentInputIteration.get(i));
+                }
+            }
+
+
+            ArrayList<Integer> finished = sortArray(fileDataJustNums,fileDataJustRules);
+            if(!finished.equals(currentInputIteration)) {
+
+                total+= finished.get(finished.size()/2);
+            }else{
             }
         }
+        System.out.println(total);
+
     }
+
+    public static ArrayList<Integer> sortArray(ArrayList<Integer> input, ArrayList<Integer> rules){
+        for(int i =0; i < input.size(); i++){
+            for(int j = 0; j < rules.size(); j++){
+
+                  //checks if rules at j index starts with the input at input index
+                  if(rules.get(j).toString().startsWith(input.get(i).toString())){
+                      int lesserThan = input.get(i);
+                      int greaterThan = Integer.parseInt(rules.get(j).toString().substring(input.get(i).toString().length()));
+
+                      //check indexes in input up to index lesserThanIndex to move items around.
+                      for(int k = 0; k < i; k++){
+                          if(input.get(k) == greaterThan){
+                              int temp = input.get(k);
+                              input.remove(k);
+                              input.add(i, temp);
+                              i--;
+                          }
+                      }
+
+                  }
+            }
+        }
+        return input;
+    }
+
     public static ArrayList<Integer> getNewIteration(ArrayList<String> inputContainer, int iteration){
         ArrayList<Integer> toReturn = new ArrayList<>();
         for(int i =0; i < inputContainer.get(iteration).split(",").length; i++){
