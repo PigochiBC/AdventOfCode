b83d894132710264e15cefc668f8a67921621899
Dashiel Lareau <133545504+PigochiBC@users.noreply.github.com>
2024-12-13 06:36:10 -0500

    Add files via upload

--- Day5/solve.java
+++ Day5/solve.java
@@ -1,46 +1,58 @@
+package Day5;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class solve {
+    static boolean rulesSwitch = false;
+    static ArrayList<String> inputStrings = new ArrayList<>();
     public static void main(String[] args){
-        ArrayList<String> fileData = getFileData("input.txt");
-        String[] fileDataSplit = fileData.getFirst().split(" ");
-        ArrayList<Long> fileDataAsLongs = new ArrayList<>();
-        for(String el: fileDataSplit){
-            fileDataAsLongs.add(Long.parseLong(el));
-        }
+        ArrayList<String> fileData = getFileData("Day5/input.txt");
+        ArrayList<Integer> fileDataJustNums = new ArrayList<>();
+        ArrayList<Integer> fileDataJustRules = new ArrayList<>();
+        ArrayList<Integer> currentInputIteration = getNewIteration(inputStrings, 0);
+        System.out.println(currentInputIteration);
         /*
-            Rules in this order:
-            Stones that are 0 : Turn into 1
-            Stones with even number of digits: split into 2 stones (Middle Left digit, middle right digit), leading 0's not kept.
-            Neither 0 nor even digits: multiplied by 2024.
+         * Algorithm:
+         *   1. Get just nums in array to its own arraylist
+         *   2. Remove irrelevent info from nums and rules by comparing to current iteration of input
+         *   3. 
          */
-        for(int i = 0 ; i < 25; i ++){
-            for(int j = 0; j < fileDataAsLongs.size(); j++){
-                //rule 1
-                if(fileDataAsLongs.get(j)==0L){
-                    fileDataAsLongs.set(j, 1L);
-                }
-                //rule 2
-                else if(fileDataAsLongs.get(j).toString().length()%2==0L){
+        
+        for(int i =0; i < fileData.size(); i++){
+            if(currentInputIteration.contains(Integer.parseInt(fileData.get(i).split("\\|")[0])) && currentInputIteration.contains(Integer.parseInt(fileData.get(i).split("\\|")[1]))){
+                fileDataJustRules.add(Integer.parseInt(fileData.get(i).split("\\|")[0] + "" + fileData.get(i).split("\\|")[1]));
+                fileDataJustNums.add(Integer.parseInt(fileData.get(i).split("\\|")[0]));
+            } 
+        }
+        
+        System.out.println(fileDataJustRules);
+        System.out.println(fileDataJustNums);
+    }
+    public static void sortArray(ArrayList<Integer> input, ArrayList<Integer> rules){
+        for(int i =0; i < input.size(); i++){
 
-                    String temp = fileDataAsLongs.get(j).toString();
-                    //System.out.println(j +" " + fileDataAsLongs);
-                    Long leftHalf = Long.parseLong(temp.substring(0,temp.length()/2));
-                    Long rightHalf = Long.parseLong(temp.substring(temp.length()/2));
-                    fileDataAsLongs.set(j, leftHalf);
-                    fileDataAsLongs.add(j+1,rightHalf);
-                    j++;
-                }
-                //rule 3
-                else{
-                    fileDataAsLongs.set(j, fileDataAsLongs.get(j)*2024);
-                }
+            for(int j = 0; j < input.size(); j++){
+                //if we find the rule number at index j, check if i > j, telling us if the rule is being followed or not.
+                if(rules.get(i) == input.get(j)){
+                    if(i > j){
+                        int temp = input.get(i);
+                        input.set(i, input.get(j));
+                        input.set(j, temp);
+                    }
+                } 
             }
         }
-        System.out.println(fileDataAsLongs.size());
+    }
+    public static ArrayList<Integer> getNewIteration(ArrayList<String> inputContainer, int iteration){
+        ArrayList<Integer> toReturn = new ArrayList<>();
+        for(int i =0; i < inputContainer.get(iteration).split(",").length; i++){
+            toReturn.add(Integer.parseInt(inputContainer.get(iteration).split(",")[i]));
+        }
+        return toReturn;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
@@ -50,10 +62,15 @@ public class solve {
             Scanner s = new Scanner(f);
             while (s.hasNextLine()) {
                 String line = s.nextLine();
-                if (!line.equals(""))
+                if (!line.equals("") && !rulesSwitch)
                     fileData.add(line);
+                else if(rulesSwitch){
+                    inputStrings.add(line);
+                }
+                else{
+                    rulesSwitch = true;
+                }
             }
-            s.close();
             return fileData;
         }
         catch (FileNotFoundException e) {
