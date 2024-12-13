92d234c9aa8ed636d799422d1db041cf28c108ef
Dashiel Lareau <133545504+PigochiBC@users.noreply.github.com>
2024-12-13 14:49:28 -0500

    Add files via upload

--- Day11/solve.java
+++ Day11/solve.java
@@ -10,42 +10,47 @@ public class solve {
         String[] fileDataSplit = fileData.getFirst().split(" ");
         HashMap<Long, Long> fileDataAsLongs = new HashMap<>();
         for(String el: fileDataSplit){
-            fileDataAsLongs.put(Long.parseLong(el), 2L);
+            fileDataAsLongs.put(Long.parseLong(el), 1L);
         }
         fileDataAsLongs.put(1L, 0L);
+        fileDataAsLongs.put(0L, 0L);
+        fileDataAsLongs = calcBlinks(fileDataAsLongs, 0);
+        long total = 0;
+        for(long x : fileDataAsLongs.keySet()){
+            total+=fileDataAsLongs.get(x);
+        }
+        System.out.println(total);
 
-        calcBlinks(fileDataAsLongs, 0);
     }
     public static HashMap<Long,Long > calcBlinks(HashMap<Long, Long> inputHash, int blinks){
-        if(blinks==75){
+        if(blinks==6){
             return inputHash;
         }
         HashMap<Long, Long> newHashMap = new HashMap<>(inputHash);
         for(Long key : inputHash.keySet()){
-            if(key == 0 && inputHash.get(0L) > 0){
-                newHashMap.replace(1L, inputHash.get(0L));
-                newHashMap.replace(0L, 0L);
-            }else if(key.toString().length()%2 == 0L && inputHash.get(key) > 0){
+            long newHashKey = newHashMap.get(key);
+            if(key == 0 ){
+                newHashMap.put(1L,inputHash.get(key)+newHashMap.get(key));
+                newHashMap.put(0L, newHashKey-inputHash.get(key));
+
+            }else if(key.toString().length()%2 == 0L){
                 long key1 = Long.parseLong(key.toString().substring(key.toString().length() / 2));
                 long key2 = Long.parseLong(key.toString().substring(0,key.toString().length()/2));
 
-                newHashMap.putIfAbsent(key1, inputHash.get(key));
-                newHashMap.putIfAbsent(key2, inputHash.get(key));
 
-                newHashMap.replace(key1,inputHash.get(key));
-                newHashMap.replace(key2, inputHash.get(key));
-                newHashMap.replace(key, 0L);
+                newHashMap.put(key1, inputHash.get(key) + newHashMap.getOrDefault(key1,0L));
+                newHashMap.put(key2, inputHash.get(key) + newHashMap.getOrDefault(key2, 0L));
+                newHashMap.put(key, newHashKey-inputHash.get(key));
             }
             //rule 3
-            else if(inputHash.get(key) > 0){
-                newHashMap.putIfAbsent(2024L*key, inputHash.get(key));
-                newHashMap.replace(2024L*key, inputHash.get(key));
-                newHashMap.replace(key, 0L);
+            else {
+                newHashMap.put(2024L*key, inputHash.get(key) + newHashMap.getOrDefault(2024*key,0L));
+                newHashMap.put(key, newHashKey-inputHash.get(key));
 
             }
 
         }
-        System.out.println(inputHash);
+        System.out.println(newHashMap);
         return calcBlinks(newHashMap, blinks+1);
     }
 
